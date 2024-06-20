b8af3939bae2d00669cc11602466b502f50dc2f4
baha-brahim <baha.brahim@isi.utm.tn>
2024-06-20 12:47:13 +0100

    Did exercices from 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array , ...removes) {
+	for (let i = 0 ; i<removes.length ;i++) {
+		let index = array.indexOf(removes[i]);
+		while (index !== -1) {
+			array.splice(index , 1);
+			index = array.indexOf(removes[i]);
+		}
+	}
+	return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
