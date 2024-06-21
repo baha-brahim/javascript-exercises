9e5e13dbba235fbaad64a8904f6e9d3ae77cda1d
baha-brahim <baha.brahim@isi.utm.tn>
2024-06-21 17:04:09 +0100

    commiting the 4th 5th 6th

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+	if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+	if (x < 0 || y < 0 ) return "ERROR";
+	let sum = 0;
+	if (x > y) {
+		let aux = x ;
+		x = y ;
+		y  = aux ;
+	}
+	for (let i = x ; i<=y ; i++) {
+		sum+=i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
