b7eb0eb8ab4b4b2ca16bd465287a990ca0bb51e9
baha-brahim <baha.brahim@isi.utm.tn>
2024-06-22 17:00:37 +0100

    did the 8th and the 9th

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...nums) {
+    return nums[0] + nums[1] ;
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+    return nums[0] - nums[1] ; 
 };
 
-const sum = function() {
+const sum = function(nums) {
 	
+    let sum = 0 ;
+    for (let i = 0 ; i<nums.length ; i++) {
+        sum += nums[i] ;
+    }
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
 
+    let prod = 1 ;
+    for (let i = 0 ; i<nums.length ; i++) {
+        prod *= nums[i] ;
+    }
+    return prod;
 };
 
-const power = function() {
-	
+const power = function(...nums) {
+    let pow = 1 ;
+    for (let i = 0 ; i<nums[1] ; i++) {
+        pow *= nums[0];
+    }
+    return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(...nums) {
+    let fact = 1 ;
+    for (let i = 1 ; i<=nums[0] ; i++) {
+        fact *= i ;
+    }
+    return fact;
 };
 
 // Do not edit below this line
