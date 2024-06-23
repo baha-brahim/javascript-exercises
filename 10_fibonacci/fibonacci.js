c07aba86d46cbb1a4972006813cb313c56de12f6
baha-brahim <baha.brahim@isi.utm.tn>
2024-06-23 13:45:53 +0100

    did all of em

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (!isNaN(parseInt(num))) {
+        num = Number(num) ;
+    } else return "OOPS" ;
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0 ;
+    if (num === 1) return 1 ;
+    let u0 = 1 ;
+    let u1 = 1;
+    while (num-2) {
+        let aux = u1 ;
+        u1 = u0 + u1;
+        u0 = aux;
+        num--;
+    }
+    return u1;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let today = new Date() ;
+    let year = today.getFullYear();
 
+    let max = {'age':0 , 'name':'test'}   ;
+    for (let i = 0 ; i<people.length ; i++) {
+        if (!('yearOfDeath' in people[i])) {
+            people[i].yearOfDeath = year ;
+        }
+        age = people[i].yearOfDeath - people[i].yearOfBirth ;
+        if (max.age < age) {
+            max.name = people[i].name; max.age = age;
+        }
+        console.log(max);
+    }
+    return max ;
+    
 };
 
 // Do not edit below this line
